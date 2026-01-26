72a4b3bfe37fa19648094b98e8566461116f5ebc
Varun Goyal <varungoyalkkp1@gmail.com>
2026-01-26 19:04:41 +0530

    feat:seat seeder added

--- src/models/airplane.js
+++ src/models/airplane.js
@@ -16,7 +16,11 @@ module.exports = (sequelize, DataTypes) => {
         foreignKey: 'airplaneId',
         onDelete: 'CASCADE'
       });
-//
+      this.hasMany(models.Seat, {
+        foreignKey: 'airplaneId',
+        onDelete: 'CASCADE'
+      });
+
     }
   }
  Airplane.init({
